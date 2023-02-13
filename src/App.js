da76bba166c84196531e0e81be338f5f574cf495
LukasdeSouza <lucasdesouzasilva112@gmail.com>
2023-02-13 16:01:32 -0300

    adding protectedRoute to the project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
+import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CustomersPage from "./pages/customers";
 import CustomerDescriptionPage from "./pages/customers/description";
 import NewCustomerPage from "./pages/customers/new";
 import DashboardPage from "./pages/dashboard";
 import LoginComponent from "./pages/login";
+import { ProtectedRoute } from "./utils/protectedRoutes";
 
-export default function Login() {
+export default function App() {
+  const [user, setUser] = useState(null)
+
+  const handleLogin = () => setUser({ id: '1', name: 'robin' })
+  const handleLogout = () => setUser(null)
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/login' element={<LoginComponent />} />
-        <Route path='/dashboard' element={<DashboardPage />} />
-        <Route path='/customers' element={<CustomersPage />} />
-        <Route path='/customers-new' element={<NewCustomerPage />} />
-        <Route path='/customers-description' element={<CustomerDescriptionPage />} />
+        <Route path='/dashboard' element={
+          <ProtectedRoute user={user}>
+            <DashboardPage />
+          </ProtectedRoute>
+        } />
+        <Route path='/customers' element={
+          <ProtectedRoute user={user}>
+            <CustomersPage />
+          </ProtectedRoute>
+        } />
+        <Route path='/customers-new' element={
+          <ProtectedRoute user={user}>
+            <NewCustomerPage />
+          </ProtectedRoute>
+        } />
+        <Route path='/customers-description' element={
+          <ProtectedRoute user={user}>
+            <CustomerDescriptionPage />
+          </ProtectedRoute>
+        } />
       </Routes>
     </BrowserRouter>
   )

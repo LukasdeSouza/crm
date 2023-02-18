2b879bb48b6bdde775418c9fa47f451feecded59
LukasdeSouza <lucasdesouzasilva112@gmail.com>
2023-02-18 19:57:03 -0300

    fixing login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,17 +1,9 @@
 import { LockClosedIcon } from '@heroicons/react/20/solid'
-import { useContext } from 'react'
-import CustomerController from '../../controllers/CustomerController'
-import RootStoreContext from '../../rootStore'
-import { customerInfos } from '../../queries/customers'
-import { client } from '../../services/graphql.service'
 import { useNavigate } from 'react-router-dom'
 import { ROOT_PASSWORD, ROOT_USER } from '../../utils/constants'
 
 
 const LoginComponent = () => {
-  const { customerStore } = useContext(RootStoreContext)
-  const controller = new CustomerController()
-
   const navigate = useNavigate()
 
   const userAuthentication = (e) => {
@@ -37,21 +29,15 @@ const LoginComponent = () => {
             alt="Your Company"
           />
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-            Sign in to your account
+            Faça Login na sua Conta
           </h2>
-          <p className="mt-2 text-center text-sm text-gray-600">
-            Or{' '}
-            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-              start your 14-day free trial
-            </a>
-          </p>
         </div>
         <form className="mt-8 space-y-6" action="#" method="POST">
           <input type="hidden" name="remember" defaultValue="true" />
           <div className="-space-y-px rounded-md shadow-sm">
             <div>
               <label htmlFor="email-address" className="sr-only">
-                Email address
+                Email
               </label>
               <input
                 id="email-address"
@@ -60,12 +46,12 @@ const LoginComponent = () => {
                 autoComplete="email"
                 required
                 className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                placeholder="Email address"
+                placeholder="Endereço de Email"
               />
             </div>
             <div>
               <label htmlFor="password" className="sr-only">
-                Password
+                Senha
               </label>
               <input
                 id="password"
@@ -74,27 +60,15 @@ const LoginComponent = () => {
                 autoComplete="current-password"
                 required
                 className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                placeholder="Password"
+                placeholder="Senha"
               />
             </div>
           </div>
 
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <input
-                id="remember-me"
-                name="remember-me"
-                type="checkbox"
-                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-              />
-              <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-                Remember me
-              </label>
-            </div>
-
+          <div className="flex items-center">
             <div className="text-sm">
               <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                Forgot your password?
+                Esqueceu sua senha?
               </a>
             </div>
           </div>
@@ -108,7 +82,7 @@ const LoginComponent = () => {
               <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                 <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
               </span>
-              Sign in
+              Entrar
             </button>
           </div>
         </form>

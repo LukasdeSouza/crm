c72ff96820a138f645f380780427386e6406a9f0
Lucas de Souza <lucas@bestdeal.com.br>
2023-03-06 13:53:23 -0300

    fixing some issues

--- src/components/loginButton/index.js
+++ src/components/loginButton/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { LockClosedIcon } from '@heroicons/react/20/solid'
+
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return <button
+    className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+    onClick={() => loginWithRedirect()}
+  >
+    <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+      <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+    </span>
+    Login
+  </button>;
 };
 
 export default LoginButton;
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -77,7 +77,7 @@ const LoginComponent = () => {
               </span>
               Entrar
             </button>
-            <LoginButton />
+            {/* <LoginButton /> */}
           </div>
         </form>
       </div>

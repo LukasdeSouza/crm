fee285e5198948d87977ad767628e18f11c09e3c
Lucas de Souza <lucas@bestdeal.com.br>
2023-03-10 19:33:22 -0300

    improving the application

--- src/components/pageHeader/index.js
+++ src/components/pageHeader/index.js
@@ -34,7 +34,6 @@ export default function PageHeader({ title, children, customers }) {
   return (
     <>
       <div className="min-h-full">
-        <LogoutButton />
         <Disclosure as="nav" className="bg-gray-800">
           {({ open }) => (
             <>
@@ -184,7 +183,7 @@ export default function PageHeader({ title, children, customers }) {
 
         <header className="bg-white shadow">
           <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8 flex flex-row justify-between">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">{title}</h1>
+            <h3 className="text-3xl font-bold tracking-tight text-gray-900">{title}</h3>
             {customers
               &&
               <button type="button"
